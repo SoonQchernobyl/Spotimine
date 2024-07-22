9f0b79ac1f9e00ca1f1b2cbaab8a2574fdc12976
SoonQchernobyl <113972823+SoonQchernobyl@users.noreply.github.com>
2024-07-22 18:36:56 +0900

    building index

---
+++ app/welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My App!</h1>
+      <p>This is the index page.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ ui/loginButton.tsx
@@ -0,0 +1,9 @@
+export default function ContinueButton() {
+  return (
+  /* Rectangle 1 */
+<div style={{position: "absolute", width: "337px", height: "49px", left: "46px", top: "605px", background: "#1ED760", borderRadius: "45px"}}>
+
+</div>
+
+}
+
